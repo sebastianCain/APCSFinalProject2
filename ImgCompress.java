0a1daad2fe5296adad56dbd41a20fd4073e8e1ab
Asher Lasday <asherlasday@Ashers-MacBook-Air.local>
2016-05-31 19:00:54 -0400

    my name is asher and im an idiot

---
+++ ImgCompress.java
@@ -0,0 +1,14 @@
+public class ImgCompress{
+
+	BufferedImage img = Image IO.read(new File("test.bmp"));
+	
+	int _width = img.getWidth();
+	int _height = img.getHeight();
+
+	byte[][] pixels = new byte[][];
+
+
+	public static void main(String[] args){
+	}
+}
+
