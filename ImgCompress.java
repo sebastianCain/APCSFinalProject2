b8703e2cc44ab03dc1872bc3bfe3db9a76de3b51
Asher Lasday <asherlasday@Ashers-MacBook-Air.local>
2016-05-31 19:06:07 -0400

    sebastian is a bitch

--- ImgCompress.java
+++ ImgCompress.java
@@ -1,11 +1,18 @@
+
+package main;
+
+import java.nio.ByteBuffer;
+
 public class ImgCompress{
 
-	BufferedImage img = Image IO.read(new File("test.bmp"));
-	
+	BufferedImage img = Image IO.read(new File("sample.bmp"));
+
 	int _width = img.getWidth();
 	int _height = img.getHeight();
 
-	byte[][] pixels = new byte[][];
+	byte[][] pixels = new byte[_height][_width];
+
+	byte[] palette = new byte[];
 
 
 	public static void main(String[] args){
