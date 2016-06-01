1eee44348e458e01c7deedaa953d70de016495c8
Asher Lasday <asherlasday@Ashers-MacBook-Air.local>
2016-05-31 23:18:50 -0400

    hypothetically i coulda macked tho

--- ImgCompress.java
+++ ImgCompress.java
@@ -28,15 +28,35 @@ public class ImgCompress{
 
 	byte[][] pixels;
 
-	//byte[] palette = new byte[];
+	byte[] palette;
 
+	public static void makePalette(){
+		ArrayList<Integer> clrs = new ArrayList<>();
 
+		for (int x = 0; x < pixels.length; x++){
+			for (int y = 0; y < pixels[x].length; y++){
+				int color = image.getRGB(x, y);
+		    	if (!clrs.contains(color)) clrs.add(color);
+		    }
+		}
+		byte[] palette = new byte[clrs.size()];
+		for (int i = 0; i < palette.size(); i++{
+			palette[i] = palette.get(i);
+		}
+	}
+
+	
 	public static void main(String[] args){
 		setupImg();
 		int _width = _img.getWidth();
 		int _height = _img.getHeight();
 		System.out.println("width: " + _width + "height: " + _height);
+
 		byte[][] pixels = new byte[_height][_width];
+
+		
+            }
+        }
 	}
 }
 
